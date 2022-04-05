664e77b791e7b685d785115f467f61f38ec31358
YipTszChiu <515061462@qq.com>
2022-04-05 18:47:27 +0800

    2022.04.05 peers

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -26,6 +26,7 @@ type Group struct {
 	name      string
 	getter    Getter //缓存未命中时的回调callback
 	mainCache cache  //前面实现的并发缓存
+	peers     PeerPicker
 }
 
 var (
@@ -46,9 +47,9 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 
 	//新建一个Group
 	g := &Group{
-		name,
-		getter,
-		cache{cacheBytes: cacheBytes},
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
 	}
 	//将这个Group加入到map映射中
 	groups[name] = g
@@ -79,8 +80,37 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
+// RegisterPeers()方法，将 实现了 PeerPicker 接口的 HTTPPool 注入到 Group 中。
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	// 如果已有注入过的PeerPicker，报错
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+
+	g.peers = peers
+}
+
+// 使用 PickPeer() 方法选择节点，若非本机节点，则调用 getFromPeer() 从远程获取。若是本机节点或失败，则回退到 getLocally()
 func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err = g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer", err)
+		}
+	}
+
 	return g.getLocally(key)
+}
+
+// 使用实现了 PeerGetter 接口的 httpGetter 从访问远程节点，获取缓存值。
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{bytes}, nil
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
--- geecache/http.go
+++ geecache/http.go
@@ -1,19 +1,31 @@
 package geecache
 
 import (
+	"Learning_Code/geecache/consistenthash"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_geecache/"
+const (
+	defaultBasePath = "/_geecache/"
+	defaultReplicas = 50
+)
+
+// 服务端
 
 // HTTPPool implements PeerPicker for a pool of HTTP peers.
 type HTTPPool struct {
 	// this peer's base URL, e.g. "https://example.net:8000"
-	self     string //记录自己的地址，包括主机名/IP和端口
-	basePath string //节点间通讯地址的前缀，默认是/_geecache/
+	self        string                 // 记录自己的地址，包括主机名/IP和端口
+	basePath    string                 // 节点间通讯地址的前缀，默认是/_geecache/
+	mu          sync.Mutex             // 保护peers和httpGetters
+	peers       *consistenthash.Map    // 根据具体的key选择节点
+	httpGetters map[string]*httpGetter // 映射远程节点与对应的httpGetter e.g. "http://10.0.0.2:8008"
 
 	//那么 http://example.com/_geecache/ 开头的请求，就用于节点间的访问。
 	//因为一个主机上还可能承载其他的服务，加一段 Path 是一个好习惯。比如，大部分网站的 API 接口，一般以 /api 作为前缀
@@ -22,8 +34,8 @@ type HTTPPool struct {
 // NewHTTPPool initializes an HTTP pool of peers.
 func NewHTTPPool(self string) *HTTPPool {
 	return &HTTPPool{
-		self,
-		defaultBasePath,
+		self:     self,
+		basePath: defaultBasePath,
 	}
 }
 
@@ -74,3 +86,67 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//使用 w.Write() 将缓存值作为 httpResponse 的 body 返回
 	w.Write(view.ByteSlice())
 }
+
+// 实例化一致性哈希，并添加节点
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	// 初始化一个一致性哈希的Map，并调用Add函数增加节点
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	// 建立每个peer与httpGetter的映射
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+// 实现PeerPicker接口，通过key选择对应的peer，返回节点对应的 HTTP 客户端。
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	// 通过一致性哈希环找到应该读取的节点
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
+
+// 客户端
+
+type httpGetter struct {
+	baseURL string //baseURL 表示将要访问的远程节点的地址，例如 http://example.com/_geecache/
+}
+
+// 实现PeerGetter接口的Get方法
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	// 使用 http.Get() 方式获取返回值，并转换为 []bytes 类型。
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
